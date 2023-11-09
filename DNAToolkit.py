3e136ae602d34b29603312edf7c19d46b45dab4d
Carol <caroline.regis@devmagic.com.br>
2023-11-08 22:44:32 -0300

    Feat(*): implemented first functions for DNA manipulation

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,14 +1,31 @@
+import re
+
 # DNA toolkit file
 
 nucleotides = ["A", "C", "G", "T"]
+doubleStrandDNAComplement = {"A": "T", "T": "A", "G": "C", "C": "G"}
+RNAnucleotides = {"A": "U", "T": "U", "G": "C", "C": "G"}
 
 # Check the sequence to ensure it's a valid DNA string
 # A valid DNA string contains only ACTG elements
-
 def validateSeq(dna_seq):
   tmpseq = dna_seq.upper() # DNA nucleotides are written in uppercase
   for nuc in tmpseq:
     if nuc not in nucleotides:
       return False
   return tmpseq;
-    
+
+# Counting nucleotides frequency (how many times it appears in the array)    
+def countNucFrequency(seq):
+  tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0} # initial object
+  for nuc in seq: #loops through each nucleotide in the array
+    tmpFreqDict[nuc] += 1 # adds an index in the object element relative to the nucleotide that's just been counted
+  return tmpFreqDict
+
+# Generating complementary DNA strand
+def complementaryStrand(seq):
+  return ''.join([doubleStrandDNAComplement[nuc] for nuc in seq])
+
+# DNA Transcription
+def transcription(seq):
+ return ''.join([RNAnucleotides[nuc] for nuc in seq])
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from DNAToolkit import *
+import random
 
-# Getting a random DNA String
-rndDNAStr = "ATTTC"
+# Setting a DNA String
+dnaStr = "ATTTC"
+
+# Creating a random DNA String
+# random runs 20 times through the nucleotides array and gets one item each time
+randDNAStr = ''.join([random.choice(nucleotides)
+                      for nuc in range(80)])
+
+# Sequence length
+print(f" [1] + Sequence length: {len(randDNAStr)}\n")
 
 # Validating sequence
-print (validateSeq(rndDNAStr))
+print(f" [2] + Validating sequence: {validateSeq(randDNAStr)}\n")
+
+# Counting nucleotides
+print(f" [3] + Counting nucleotides: {countNucFrequency(randDNAStr)}\n")
+
+# Generating complementary strand for DNA
+print (f" [4] + Generating complementary strand to DNA:\n5' {randDNAStr} 3'")
+print (f"   {''.join(['|' for c in range(len(randDNAStr))])}")
+print (f"3' {complementaryStrand(randDNAStr)} 5'\n")
+
+# Transcription 
+print(f" [5] + DNA transcription {transcription(randDNAStr)}")
